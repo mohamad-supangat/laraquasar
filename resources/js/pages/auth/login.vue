147fbb029d1cff44d2660771cf6df8129809609e
Mohamad Supangat <hantamkoding@gmail.com>
2023-08-26 14:04:16 +0700

    add: plugin: laravel fortify

---
+++ resources/js/pages/auth/login.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui
+        consequatur quibusdam tempora illum. Commodi eos distinctio quos
+        asperiores explicabo, fuga sed sunt suscipit corrupti labore alias,
+        voluptas cum iste quam.
+    </div>
+</template>
