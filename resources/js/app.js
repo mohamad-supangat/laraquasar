0f5f93e5cdf4254166d4532977b65dcd4125ca5c
Mohamad Supangat <hantamkoding@gmail.com>
2023-10-30 11:41:13 +0700

    Copy some code and modification from laravel breeze init
    all modification after running php artisan breeze:install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,37 @@
 import "@quasar/extras/material-icons/material-icons.css";
 import "@quasar/extras/mdi-v6/mdi-v6.css";
 import "quasar/src/css/index.sass";
+import "./bootstrap";
+import "../css/app.css";
 
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { Quasar } from "quasar";
 import quasarIconSet from "quasar/icon-set/svg-mdi-v6";
 import { createApp, h } from "vue";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
-            `./pages/${name}.vue`,
-            import.meta.glob("./pages/**/*.vue"),
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue"),
         ),
-
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(Quasar, {
                 plugins: {}, // import Quasar plugins and add here
                 iconSet: quasarIconSet,
                 config: {}, // quasar config see: https://quasar.dev/start/vite-plugin/
             })
+            .use(ZiggyVue, Ziggy)
             .mount(el);
+    },
+    progress: {
+        color: "#4B5563",
     },
 });
