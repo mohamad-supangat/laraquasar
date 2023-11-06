644a03036226bd0db67602ffb76d2587908f441f
Mohamad Supangat <hantamkoding@gmail.com>
2023-11-06 11:59:05 +0700

    rewritting dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,22 +3,14 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 </script>
 
-<template>
-    <Head title="Dashboard" />
+<template lang="pug">
+Head(title="Register")
+AuthenticatedLayout
+    q-page(padding)
+        .row
+            .col-12.col-md-4
+                q-card
+                    q-card-section.text-center
+                        .text-h6 Welcome to LaraQuasar
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
-            </div>
-        </div>
-    </AuthenticatedLayout>
 </template>
