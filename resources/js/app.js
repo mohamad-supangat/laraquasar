b1999801633164e1db8579ba15206185932373d5
Mohamad Supangat <hantamkoding@gmail.com>
2023-11-06 11:31:05 +0700

    wip: remake authenticated layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import "@quasar/extras/material-icons/material-icons.css";
 import "@quasar/extras/mdi-v6/mdi-v6.css";
 import "quasar/src/css/index.sass";
 import "./bootstrap";
-import "../css/app.css";
 
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     plugins: [
         laravel({
-            input: "resources/js/app.js",
+            input: ["resources/js/app.js", "resources/css/app.scss"],
         }),
         vue({
             template: { transformAssetUrls },
